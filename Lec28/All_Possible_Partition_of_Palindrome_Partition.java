c2863011d4684788377e92448b1323ea0122e653
sahil7891 <168364450+sahil7891@users.noreply.github.com>
2025-04-25 00:58:29 +0530

    Recursion & Backtracking 4

---
+++ Lec28/All_Possible_Partition_of_Palindrome_Partition.java
@@ -0,0 +1,24 @@
+package Lec28;
+
+public class All_Possible_Partition_of_Palindrome_Partition { 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "nitin";
+		String ans = " ";
+		PrintPartition(str,ans);
+	}
+	public static void PrintPartition(String ques, String ans) {
+		if(ques.length()==0) {//base case
+			System.out.println(ans); 
+		}
+		for(int i =1; i<=ques.length(); i++) { //str(ques) ki jitni length hogi utni hi recursive call lgegi isliye for loop use kiya h
+	
+			PrintPartition(ques.substring(i),ans+ ques.substring(0,i)+" ,"); //0,i => substring ka index no. 0 h // i varie kr kr rha hai code ke acording    
+		}
+
+	}
+
+}
+// is code me all possible partion ho rhe hai and Palindrome_Partition wale code me sirf palindrome partition wala code run hoga
+
